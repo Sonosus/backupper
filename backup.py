b088cbeb29226dd1b309d249fe476c808b48d1ce
Sonosus <sonosusoshwa@gmail.com>
2022-04-25 16:52:41 +0100

    formatting

--- backup.py
+++ backup.py
@@ -1,14 +1,9 @@
-import os
-from datetime import datetime
+import os; from datetime import datetime
 name = "sonosus" # enter username here
-print("Sonosus' Quick And Dirty Backup Script")
-print("Current Time:")
-timenow= datetime.now()
+timenow = datetime.now()
 stamp = timenow.strftime("%d-%b-%Y-%H-%M-%S ")
-print(stamp)
-args = "--exclude-from '/home/" + name + "/backupignore.txt' -v"
-command = ("/usr/bin/rclone sync /home/" + name + " onedrive:backup/" + stamp + args)
-print("Starting Backup!")
+print("Sonosus' Quick And Dirty Backup Script \n Current Time:" + stamp + "\n Starting Backup!")
+command = ("/usr/bin/rclone sync /home/" + name + " onedrive:backup/" + stamp + "--exclude-from '/home/" + name + "/.backupignore.txt' -v")
 os.chdir("/home/"+ name)
 os.system(command)
-print("Backup Complete!")
+
